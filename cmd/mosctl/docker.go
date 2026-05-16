6ad9b54a8e94b60e18115953bff35556cbd1b4c1
KyleYu2024 <kyleyu2008@gmail.com>
2026-05-16 09:17:17 +0800

    Bump Docker release to v0.5.2

--- cmd/mosctl/docker.go
+++ cmd/mosctl/docker.go
@@ -32,7 +32,7 @@ func init() {
 
 func runDockerPanel() {
 	fmt.Println("=====================================")
-	fmt.Println("             MosCtl Docker (v0.5.1)  ")
+	fmt.Println("             MosCtl Docker (v0.5.2)  ")
 	fmt.Println("=====================================")
 
 	os.Setenv("MOSCTL_MODE", "docker")
